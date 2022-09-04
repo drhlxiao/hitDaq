cda466f942ceb72258852aef62724df07e52bb98
dr.hualinxiao@gmail.com <dr.hualinxiao@gmail.com>
2022-09-04 23:04:27 +0200

    add hv config

--- core/calibration.py
+++ core/calibration.py
@@ -2,7 +2,9 @@ def convert_HV_real_to_raw(hv_value):
     """
     convert real HV value (V) to raw value to be written to register 
     """
-    raw_value=int(hv_value)
+    offset=0
+    slope=1
+    raw_value=int(offset+slop*hv_value)
 
     return raw_value
 
